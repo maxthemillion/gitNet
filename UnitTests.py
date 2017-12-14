589fc731aef57af472f1cc009fa938399d21ac54
maxthemillion <max.rahn@tum.de>
2017-12-14 12:28:18 +0100

    Add unit test file and Neo4j controller

---
+++ UnitTests.py
@@ -0,0 +1,15 @@
+import unittest
+from Thread_class import Thread
+
+class ThreadTests(unittest.TestCase):
+    def test_recognize_references_relaxed(self):
+        self.failUnless(False)
+
+
+
+
+def main():
+   unittest.main()
+
+if __name__ == '__main__':
+    main()
