96fb7f5b9525f62d35c2400d2af1341a79abf975
maxthemillion <max.rahn@tum.de>
2018-02-28 15:06:55 +0100

    added month filter

--- DataPreparation/prepare_commits.py
+++ DataPreparation/prepare_commits.py
@@ -15,8 +15,11 @@ in_filename = "/Volumes/MyBook Max/03_Studium/TUM/S5_WS1718/MA/Data/GHT/mysql-20
 out_filename = "commits_filtered.csv"
 
 
-min_year = 2015
-max_year = 2018
+min_year = 2016
+max_year = 2016
+
+min_month = 1
+max_month = 1
 
 proj_keys = set(line.strip() for line in open('repo_ids.csv'))
 # proj_keys = set(['6227'])
@@ -30,21 +33,25 @@ with open(in_filename) as in_f, open(out_filename, 'w') as out_f:
 
         if counter % 1000000 == 0:
             print(time.time()-lap)
-            print (counter)
+            print(counter/1000000)
             lap = time.time()
 
-        date_pos1 = len(line) - 21
-        date_posend = date_pos1 + 4
+        year_pos1 = len(line) - 21
+        year_posend = year_pos1 + 4
+        year = int(line[year_pos1:year_posend])
 
-        proj_posend = date_pos1 - 2
-        proj_pos1 = str.rfind(line, ',', proj_posend-15, proj_posend) + 1
+        if min_year <= year <= max_year:
+            proj_posend = len(line) - 23
+            proj_pos1 = str.rfind(line, ',', proj_posend - 15, proj_posend) + 1
+            proj = line[proj_pos1:proj_posend]
 
-        proj = line[proj_pos1:proj_posend]
+            if proj in proj_keys:
+                month_pos1 = year_posend + 1
+                month_posend = month_pos1 + 2
+                month = int(line[month_pos1:month_posend])
 
-        if proj in proj_keys:
-            year = int(line[date_pos1:date_posend])
-            if min_year <= year <= max_year:
-                out_f.write(line)
+                if min_month <= month <= max_month:
+                    out_f.write(line)
 
 duration = time.time() - start
 print(duration)
